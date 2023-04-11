8d46b298b4e8bd6d0e6122e26c98e98feb53f65c
MariAnn85 <75684730+MariAnn85@users.noreply.github.com>
2023-04-11 13:45:36 +0400

    1 file added, 29 deleted

--- server/config/types.ts
+++
@@ -1,7 +0,0 @@
-export interface IConnectionDB {
-    user: string,
-    password: string,
-    host: string,
-    port: number,
-    database: string,
-}
--- server/routes/recordingRoute.ts
+++
@@ -1,6 +0,0 @@
-import express, {Router} from 'express'
-import recordingRequest from "../requests/recordingRequest";
-
-const router: Router = express.Router()
-router.post('/', recordingRequest.update)
-export default router
